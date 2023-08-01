435471ca26867158dd30604c2c9a842270c886f1
shmily <1968302904@qq.com>
2023-08-01 20:23:44 +0800

    商家完成订单功能开发

--- sky-server/src/main/java/com/sky/service/OrderService.java
+++ sky-server/src/main/java/com/sky/service/OrderService.java
@@ -109,4 +109,11 @@ public interface OrderService {
      * @param id
      */
     void delivery(Long id);
+
+    /**
+     * 完成订单
+     *
+     * @param id
+     */
+    void complete(Long id);
 }
