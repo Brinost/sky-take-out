9444e88d669450a23cdf40f0c1be7c8e8b9122e4
shmily <1968302904@qq.com>
2023-08-02 16:05:37 +0800

    工作台功能开发

---
+++ sky-server/src/main/java/com/sky/service/WorkspaceService.java
@@ -0,0 +1,52 @@
+package com.sky.service;
+
+import com.sky.vo.BusinessDataVO;
+import com.sky.vo.DishOverViewVO;
+import com.sky.vo.OrderOverViewVO;
+import com.sky.vo.SetmealOverViewVO;
+
+import java.time.LocalDateTime;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Package: com.sky.service
+ * @ClassName: WorkService
+ * @author: shmily
+ * @createTime: 2023/8/2 15:59
+ * @version: 17
+ * @Description:
+ */
+
+
+
+public interface WorkspaceService {
+
+    /**
+     * 根据时间段统计营业数据
+     * @param begin
+     * @param end
+     * @return
+     */
+    BusinessDataVO getBusinessData(LocalDateTime begin, LocalDateTime end);
+
+    /**
+     * 查询订单管理数据
+     * @return
+     */
+    OrderOverViewVO getOrderOverView();
+
+    /**
+     * 查询菜品总览
+     * @return
+     */
+    DishOverViewVO getDishOverView();
+
+    /**
+     * 查询套餐总览
+     * @return
+     */
+    SetmealOverViewVO getSetmealOverView();
+
+}
+
